d3e48ac3ec376b6e99f3ce8b69a0ba064f033cc2
HuzaifaRizwan15 <huzaifahunter04@gmail.com>
2026-04-24 11:09:39 +0500

    feature/test

--- flow.py
+++ flow.py
@@ -1,7 +1,5 @@
-VERSION = "0.1.1"
 
-def add(a, b):
-    return int(a) + int(b)  
+def subtract(a, b):
+    return int(a) - int(b)  
 
-print(add(2, "3"))
-print(f"Version: {VERSION}")
+print(add(2, 1))
